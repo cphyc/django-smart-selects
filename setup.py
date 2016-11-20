0b2d3e5624f0c01e532d78f9310b33ab944dd7af
Corentin Cadiou <contact@cphyc.me>
2016-11-20 16:59:44 +0100

    update version

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(name="django-smart-selects",
-      version="1.2.6",
+      version="1.2.6-2",
       description="Django application to handle chained model fields.",
       author="Patrick Lauber",
       author_email="digi@treepy.com",
-      url="https://github.com/digi604/django-smart-selects",
+      url="https://github.com/cphyc/django-smart-selects",
       packages=find_packages(),
       include_package_data=True,
       )
